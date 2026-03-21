02bd57ccd6d2da062a15497aad214938611ac4c0
Honningbolden <hmun@itu.dk>
2026-03-21 13:03:13 +0100

    Added todo overview for R tree implementation

--- src/main/java/org/example/Tree.java
+++ src/main/java/org/example/Tree.java
@@ -8,6 +8,20 @@ public class Tree {
     public List<Tree> nodes;
 
     public Tree _find(BoundingBox searchArea) {
+        /*
+        TODO: I need to create utility bounding boxes for all elements (not use their own bounding boxes). When I query the R-tree I need to keep traversing the tree until I hit a leaf node, and then return the leaf node. Therefore, all OSM elements (Node, Way, Relation) must be leaf nodes, in order for the R-tree to work.
+        There are two operations
+        - Insert:
+            1. Start at the root
+            2. Choose the MBR who needs the least enlargement to fit the element that will be inserted
+            3. Recurse until the element is at a leaf node
+            3. If the leaf node overflows after inserting the element, then split the node
+        - Search:
+            1. Start at root
+            2. Test each entry's MBR to see if it overlaps with search area -> If it overlaps then recurse into child
+            3.
+         */
+
         if (searchArea.isOverlappingOther(element.mbr.box())) {
             return this;
         }
