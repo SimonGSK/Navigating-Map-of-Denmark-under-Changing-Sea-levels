9926d55561f50d92a2a7e45aee7ce7e712ae4375
DanielKyhl <dmkyhl@gmail.com>
2026-04-10 16:07:43 +0200

    .

--- src/main/java/models/ui/App.java
+++ src/main/java/models/ui/App.java
@@ -87,9 +87,9 @@ public class App extends DrawingApp {
         List<Way> allWays = new ArrayList<>(parser.getOsmWayMap().values());
 
 
-        drawables.add(new HeightCurveRenderer(hcData, meanLat));                         // 1. Højdekurver - højdekurver
-       //   drawables.add(new RelationRenderer(mapData.multiPolygons, meanLat));             // 2. Relations/multipolygons - skove, søer osv.
-        //  drawables.add(new WayRenderer(mapData.standaloneWays, meanLat));                 // 3. Ways - veje, bygninger
+       //  drawables.add(new HeightCurveRenderer(hcData, meanLat));                         // 1. Højdekurver - højdekurver
+        drawables.add(new RelationRenderer(mapData.multiPolygons, meanLat));             // 2. Relations/multipolygons - skove, søer osv.
+        drawables.add(new WayRenderer(mapData.standaloneWays, meanLat));                 // 3. Ways - veje, bygninger
 
 
         long nonemptyWays = parser.getOsmWayMap().values().stream().filter(w -> w.getNodes() != null && !w.getNodes().isEmpty()).count();
